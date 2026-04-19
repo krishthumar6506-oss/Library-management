652bcacd9e59a45f94e7185b874cf17c1cacfcdf
krishthumar6506-oss <krishthumar6506@gmail.com>
2026-04-19 10:03:07 +0530

    Add new Data table in database

--- Database/library.sql
+++ Database/library.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 11, 2026 at 08:41 AM
+-- Generation Time: Apr 19, 2026 at 06:31 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -74,6 +74,27 @@ INSERT INTO `books` (`id`, `isbn`, `title`, `author`, `publisher`, `publish_date
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `book_requests`
+--
+
+CREATE TABLE `book_requests` (
+  `id` int(11) NOT NULL,
+  `student_id` int(11) NOT NULL,
+  `book_id` int(11) NOT NULL,
+  `request_date` datetime NOT NULL DEFAULT current_timestamp(),
+  `status` varchar(20) NOT NULL DEFAULT 'Pending'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `book_requests`
+--
+
+INSERT INTO `book_requests` (`id`, `student_id`, `book_id`, `request_date`, `status`) VALUES
+(1, 18, 11, '2026-04-16 23:58:16', 'Pending');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `borrow`
 --
 
@@ -95,7 +116,7 @@ INSERT INTO `borrow` (`id`, `student_id`, `book_id`, `date_borrow`, `status`) VA
 (3, 18, 8, '2026-04-01', 0),
 (4, 18, 11, '2026-04-08', 0),
 (5, 18, 11, '2026-04-11', 0),
-(6, 18, 8, '2026-04-11', 1),
+(6, 18, 8, '2026-04-11', 0),
 (7, 18, 9, '2026-04-11', 1),
 (8, 18, 9, '2026-04-11', 1),
 (9, 18, 10, '2026-04-11', 0),
@@ -103,9 +124,15 @@ INSERT INTO `borrow` (`id`, `student_id`, `book_id`, `date_borrow`, `status`) VA
 (11, 18, 7, '2026-04-11', 0),
 (12, 18, 7, '2026-04-11', 0),
 (13, 18, 11, '2026-04-11', 0),
-(14, 18, 11, '2026-04-11', 1),
+(14, 18, 11, '2026-04-11', 0),
 (15, 18, 12, '2026-04-11', 0),
-(16, 18, 12, '2026-04-11', 0);
+(16, 18, 12, '2026-04-11', 0),
+(17, 18, 11, '2026-04-16', 0),
+(18, 18, 11, '2026-04-16', 0),
+(19, 18, 7, '2026-04-16', 0),
+(20, 18, 8, '2026-04-16', 1),
+(21, 18, 11, '2026-04-10', 1),
+(22, 19, 7, '2026-04-10', 0);
 
 -- --------------------------------------------------------
 
@@ -119,20 +146,6 @@ CREATE TABLE `password_resets` (
   `otp` varchar(6) NOT NULL,
   `expires_at` datetime NOT NULL,
   `created_at` datetime NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `book_requests`
---
-
-CREATE TABLE `book_requests` (
-  `id` int(11) NOT NULL,
-  `student_id` int(11) NOT NULL,
-  `book_id` int(11) NOT NULL,
-  `request_date` datetime NOT NULL DEFAULT current_timestamp(),
-  `status` varchar(20) NOT NULL DEFAULT 'Pending'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -153,6 +166,15 @@ CREATE TABLE `payments` (
   `paid_at` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `payments`
+--
+
+INSERT INTO `payments` (`id`, `student_id`, `payment_for`, `amount`, `payment_method`, `penalty_id`, `transaction_ref`, `status`, `paid_at`) VALUES
+(1, 18, 'membership', 200.00, 'debit_card', NULL, '222', 'Completed', '2026-04-16 23:50:12'),
+(2, 18, 'security_deposit', 2330.00, 'upi', NULL, '0', 'Completed', '2026-04-19 08:40:59'),
+(3, 19, 'late_fine', 20.00, 'upi', 1, '222', 'Completed', '2026-04-19 09:31:54');
+
 -- --------------------------------------------------------
 
 --
@@ -172,6 +194,13 @@ CREATE TABLE `penalties` (
   `paid_at` datetime DEFAULT NULL,
   `created_at` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `penalties`
+--
+
+INSERT INTO `penalties` (`id`, `borrow_id`, `student_id`, `book_id`, `due_date`, `return_date`, `late_days`, `amount`, `status`, `paid_at`, `created_at`) VALUES
+(1, 22, 19, 7, '2026-04-17', '2026-04-19', 2, 20.00, 'Paid', '2026-04-19 09:31:54', '2026-04-19 09:30:32');
 
 -- --------------------------------------------------------
 
@@ -197,21 +226,22 @@ INSERT INTO `returns` (`id`, `student_id`, `book_id`, `date_return`) VALUES
 (4, 18, 9, '2026-04-11'),
 (5, 18, 9, '2026-04-11'),
 (6, 18, 8, '2026-04-11'),
-(7, 18, 8, '2026-04-11'),
-(8, 18, 8, '2026-04-11'),
-(9, 18, 9, '2026-04-11'),
 (10, 18, 10, '2026-04-11'),
 (11, 18, 7, '2026-04-11'),
 (12, 18, 7, '2026-04-11'),
 (13, 18, 11, '2026-04-11'),
-(14, 18, 11, '2026-04-11'),
-(15, 18, 10, '2026-04-11'),
 (16, 18, 7, '2026-04-11'),
 (17, 18, 9, '2026-04-11'),
 (18, 18, 8, '2026-04-11'),
 (19, 18, 11, '2026-04-11'),
 (20, 18, 12, '2026-04-11'),
-(21, 18, 12, '2026-04-11');
+(21, 18, 12, '2026-04-11'),
+(22, 18, 11, '2026-04-16'),
+(23, 18, 8, '2026-04-16'),
+(24, 18, 7, '2026-04-18'),
+(25, 18, 11, '2026-04-18'),
+(26, 18, 11, '2026-04-19'),
+(27, 19, 7, '2026-04-19');
 
 -- --------------------------------------------------------
 
@@ -235,13 +265,13 @@ CREATE TABLE `students` (
 --
 
 INSERT INTO `students` (`id`, `firstname`, `lastname`, `created_on`, `email`, `password`, `gender`, `mobile`) VALUES
-(18, 'Krish', 'Thummar', '2026-03-23', 'krishthumar6506@gmail.com', '$2y$10$1XUjYA9VAWNAIESEvrKvmuTQCSg4xPrNXesDaFN9.TRQDJ/rpYKAi', 'Male', '0989865440'),
+(18, 'Krish', 'Thummar', '2026-03-23', 'krishthumar6506@gmail.com', '$2y$10$UMNUtQJk.ei5wj.IO52xeeLThridY6GU.H0YGHjkoZr1KK3eJzQ8e', 'Male', '0989865440'),
 (19, 'Krish', 'Thummar', '2026-04-02', 'kthummar467@gmail.com', '$2y$10$MULLHfyMx8wf00.7lZu9bOBmfESUTamVwxb030mA.97JthUwP9g4a', 'Male', '4564564566'),
 (20, 'khushaal', 'vamja', '2026-04-03', 'kvamja751@rku.ac.in', '$2y$10$ERiOS8DQflGwKBQlGgHEru6mBau2XmoCcqsSn.JJLV.f2xZApI6ey', 'Male', '9016891845'),
 (21, 'Avani', 'Rathod', '2026-04-03', 'avanirathod1111@gmail.com', '$2y$10$B3ySGwa2GHtZvpkySExmau92N1wBRZ5dN7vUs2bj0OYZrXRKvYAeW', 'Female', '1234567890'),
 (22, 'Aastha', 'Patel', '2026-04-03', 'apatel785@rku.ac.in', '$2y$10$I.5p.aZ1WG9HFvV.JRwmrOQv6W1c6yogHFyKTuUzTMs2MNaglKuZW', 'Female', '9876543210'),
 (23, 'dhaval', 'kanjariya', '2026-04-08', 'dkanjariya00@gmail.com', '$2y$10$ZDcdGRJEd69FEmTBrh8FoOhnjzBn9v9gaRQcJZai096Dp93EDEAuS', 'Male', '1234567890'),
-(24, 'Page', 'Turner', '2026-04-11', 'pageturner899@gmail.com', '$2y$10$jCHWOCBEQLoVpelKrXjXd.QjmqrzrEb.R.k8jQ.lbOj5lOiekUBB6', 'Male', '1234567890');
+(26, 'page', 'turner', '2026-04-19', 'pageturner899@gmail.com', '$2y$10$f.hdWmGbGiTtFZLeMlnDe.WrqW868B7nCcR381.RUCmmx9TAJZsYW', 'Male', '9898654408');
 
 --
 -- Indexes for dumped tables
@@ -255,6 +285,12 @@ ALTER TABLE `books`
   ADD UNIQUE KEY `isbn_unique` (`isbn`);
 
 --
+-- Indexes for table `book_requests`
+--
+ALTER TABLE `book_requests`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `borrow`
 --
 ALTER TABLE `borrow`
@@ -266,12 +302,6 @@ ALTER TABLE `borrow`
 ALTER TABLE `password_resets`
   ADD PRIMARY KEY (`id`),
   ADD KEY `email` (`email`);
-
---
--- Indexes for table `book_requests`
---
-ALTER TABLE `book_requests`
-  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `payments`
@@ -308,46 +338,46 @@ ALTER TABLE `books`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
+-- AUTO_INCREMENT for table `book_requests`
+--
+ALTER TABLE `book_requests`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT for table `borrow`
 --
 ALTER TABLE `borrow`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- AUTO_INCREMENT for table `password_resets`
 --
 ALTER TABLE `password_resets`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-
---
--- AUTO_INCREMENT for table `book_requests`
---
-ALTER TABLE `book_requests`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `payments`
 --
 ALTER TABLE `payments`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `penalties`
 --
 ALTER TABLE `penalties`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `returns`
 --
 ALTER TABLE `returns`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 
 --
 -- AUTO_INCREMENT for table `students`
 --
 ALTER TABLE `students`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
